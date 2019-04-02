aa51036b1a6bed4199a2708a6fcc7612543f8a61
Lukas <Lukas@Lukas-Laptop>
2019-04-02 13:02:12 +0200

    k

---
+++ src/Server.java
@@ -0,0 +1,30 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) {
+
+	}
+
+	private int port;
+
+	public Server(int port) {
+		super();
+		this.port = port;
+	}
+public void startListening() {
+	new Thread(new Runnable() {
+		
+		@Override
+		public void run() {
+			try {
+			ServerSocket serverSocket = new ServerSocket(port);
+			Socket remoteClientSocket = serverSocket.accept();
+			}
+			
+		}
+	})
+	
+}
+}
