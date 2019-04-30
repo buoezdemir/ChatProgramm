47d51163ac6ec258abade2ef9579d142be676a0b
Lukas <Lukas@Lukas-Laptop>
2019-04-30 07:46:17 +0200

    fdffr

--- src/application/Server.java
+++ src/application/Server.java
@@ -6,7 +6,7 @@ class Server
    public static void main(String argv[]) throws Exception
       {
  
-   		 System.out.println(" Server is Running  " );
+   		 System.out.println(" Server wurde gestartet  " );
          ServerSocket mysocket = new ServerSocket(5555);
  
          while(true)
@@ -18,22 +18,7 @@ class Server
             BufferedWriter writer= 
             		new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
  
-            writer.write("*** Welcome to the Calculation Server (Addition Only) ***\r\n");            
-            writer.write("*** Please type in the first number and press Enter : \n");
-            writer.flush();
-            String data1 = reader.readLine().trim();
- 
-            writer.write("*** Please type in the second number and press Enter : \n");
-            writer.flush();
-            String data2 = reader.readLine().trim();
- 
-            int num1=Integer.parseInt(data1);
-            int num2=Integer.parseInt(data2);
- 
-            int result=num1+num2;            
-            System.out.println("Addition operation done " );
- 
-            writer.write("\r\n=== Result is  : "+result);
+            writer.write("*** Willkommen zum Chat  ***\r\n");
             writer.flush();
             connectionSocket.close();
          }
